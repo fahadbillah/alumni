b463818376ff383a3ee55cc662708ba6c0b07774
fahadbillah <fahadbillah@yahoo.com>
2015-01-20 19:12:15 +0600

    redirect to other page when logout

--- app/scripts/services/authinterceptor.js
+++ app/scripts/services/authinterceptor.js
@@ -15,27 +15,31 @@
   ]);
 })
  // .factory('AuthInterceptor',['$q','$rootScope','$location','AUTH_EVENTS','SESSION','AuthService', function ($q,$rootScope,$location,AUTH_EVENTS,SESSION,AuthService) {
-   .factory('AuthInterceptor',['$q', function ($q) {
-     return {
-       responseError: function (response) { 
-         $rootScope.$broadcast({
-           401: AUTH_EVENTS.notAuthenticated,
-           403: AUTH_EVENTS.notAuthorized,
-           419: AUTH_EVENTS.sessionTimeout,
-           440: AUTH_EVENTS.sessionTimeout
-         }[response.status], response);
+  .factory('AuthInterceptor',['$q','$rootScope','$location','AUTH_EVENTS','Session', function ($q,$rootScope,$location,AUTH_EVENTS,Session) {
+   return {
+     responseError: function (response) { 
+       $rootScope.$broadcast({
+         401: AUTH_EVENTS.notAuthenticated,
+         403: AUTH_EVENTS.notAuthorized,
+         419: AUTH_EVENTS.sessionTimeout,
+         440: AUTH_EVENTS.sessionTimeout
+       }[response.status], response);
 
 
-         if (response.status === 401) {
+       if (response.status === 401) {
+        Session.destroy();
+        $rootScope.$broadcast(AUTH_EVENTS.notAuthenticated);
+        $rootScope.currentUser = null;
 
-          Session.destroy();
-          $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
-          $scope.setCurrentUser(null);
-          $location.replace('/login');
+        var currentUrl = $location.url();
+        if (currentUrl.search('profile') >=0 || currentUrl.search('admin') >=0) {
+          $location.path('/login');
         };
 
+      };
 
-        return $q.reject(response);
-      }
-    };
-  }]);
+
+      return $q.reject(response);
+    }
+  };
+}]);
